bb0417438e56087294a101fb1b0189cf177da1e0
Dean Fox <deanfoxcd@gmail.com>
2025-02-07 19:36:52 -0500

    add arguments in cli

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log('Server is listening');
-};
+exports.serve = serve;
+function serve(port, filename, dir) {
+    console.log('Serving traffic on port', port);
+    console.log('saving/fetching cells from', filename);
+    console.log('the file is in dir', dir);
+}
